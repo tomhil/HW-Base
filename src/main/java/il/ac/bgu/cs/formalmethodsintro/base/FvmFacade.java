cc708a1f36af5a83cf7e8ec4787b925fe7cc052b
geraw <gera.weiss@gmail.com>
2019-10-25 14:03:58 +0300

    Removed the "Not implemented yet" string.

--- src/main/java/il/ac/bgu/cs/formalmethodsintro/base/FvmFacade.java
+++ src/main/java/il/ac/bgu/cs/formalmethodsintro/base/FvmFacade.java
@@ -64,7 +64,7 @@ public class FvmFacade {
      * @return {@code true} iff the action is deterministic.
      */
     public <S, A, P> boolean isActionDeterministic(TransitionSystem<S, A, P> ts) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -78,7 +78,7 @@ public class FvmFacade {
      * @return {@code true} iff the action is ap-deterministic.
      */
     public <S, A, P> boolean isAPDeterministic(TransitionSystem<S, A, P> ts) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -93,7 +93,7 @@ public class FvmFacade {
      * @return {@code true} iff {@code e} is an execution of {@code ts}.
      */
     public <S, A, P> boolean isExecution(TransitionSystem<S, A, P> ts, AlternatingSequence<S, A> e) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -110,7 +110,7 @@ public class FvmFacade {
      * {@code ts}.
      */
     public <S, A, P> boolean isExecutionFragment(TransitionSystem<S, A, P> ts, AlternatingSequence<S, A> e) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -127,7 +127,7 @@ public class FvmFacade {
      * {@code ts}.
      */
     public <S, A, P> boolean isInitialExecutionFragment(TransitionSystem<S, A, P> ts, AlternatingSequence<S, A> e) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -143,7 +143,7 @@ public class FvmFacade {
      * @return {@code true} iff {@code e} is a maximal fragment of {@code ts}.
      */
     public <S, A, P> boolean isMaximalExecutionFragment(TransitionSystem<S, A, P> ts, AlternatingSequence<S, A> e) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -157,7 +157,7 @@ public class FvmFacade {
      * @throws StateNotFoundException if {@code s} is not a state of {@code ts}.
      */
     public <S, A> boolean isStateTerminal(TransitionSystem<S, A, ?> ts, S s) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -168,7 +168,7 @@ public class FvmFacade {
      * @throws StateNotFoundException if {@code s} is not a state of {@code ts}.
      */
     public <S> Set<S> post(TransitionSystem<S, ?, ?> ts, S s) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -180,7 +180,7 @@ public class FvmFacade {
      * @throws StateNotFoundException if {@code s} is not a state of {@code ts}.
      */
     public <S> Set<S> post(TransitionSystem<S, ?, ?> ts, Set<S> c) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -194,7 +194,7 @@ public class FvmFacade {
      * @throws StateNotFoundException if {@code s} is not a state of {@code ts}.
      */
     public <S, A> Set<S> post(TransitionSystem<S, A, ?> ts, S s, A a) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -207,7 +207,7 @@ public class FvmFacade {
      * in {@code c}, when action {@code a} is selected.
      */
     public <S, A> Set<S> post(TransitionSystem<S, A, ?> ts, Set<S> c, A a) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -217,7 +217,7 @@ public class FvmFacade {
      * @return All the states in {@code Pre(s)}, in the context of {@code ts}.
      */
     public <S> Set<S> pre(TransitionSystem<S, ?, ?> ts, S s) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -229,7 +229,7 @@ public class FvmFacade {
      * @throws StateNotFoundException if {@code s} is not a state of {@code ts}.
      */
     public <S> Set<S> pre(TransitionSystem<S, ?, ?> ts, Set<S> c) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -243,7 +243,7 @@ public class FvmFacade {
      * @throws StateNotFoundException if {@code s} is not a state of {@code ts}.
      */
     public <S, A> Set<S> pre(TransitionSystem<S, A, ?> ts, S s, A a) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -257,7 +257,7 @@ public class FvmFacade {
      * @throws StateNotFoundException if {@code s} is not a state of {@code ts}.
      */
     public <S, A> Set<S> pre(TransitionSystem<S, A, ?> ts, Set<S> c, A a) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -269,7 +269,7 @@ public class FvmFacade {
      * @return All states reachable in {@code ts}.
      */
     public <S, A> Set<S> reach(TransitionSystem<S, A, ?> ts) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -286,7 +286,7 @@ public class FvmFacade {
      */
     public <S1, S2, A, P> TransitionSystem<Pair<S1, S2>, A, P> interleave(TransitionSystem<S1, A, P> ts1,
         TransitionSystem<S2, A, P> ts2) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -304,7 +304,7 @@ public class FvmFacade {
      */
     public <S1, S2, A, P> TransitionSystem<Pair<S1, S2>, A, P> interleave(TransitionSystem<S1, A, P> ts1,
         TransitionSystem<S2, A, P> ts2, Set<A> handShakingActions) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -329,7 +329,7 @@ public class FvmFacade {
      * @return Interleaved program graph.
      */
     public <L1, L2, A> ProgramGraph<Pair<L1, L2>, A> interleave(ProgramGraph<L1, A> pg1, ProgramGraph<L2, A> pg2) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -340,7 +340,7 @@ public class FvmFacade {
      */
     public TransitionSystem<Pair<Map<String, Boolean>, Map<String, Boolean>>, Map<String, Boolean>, Object>
         transitionSystemFromCircuit(Circuit c) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -356,7 +356,7 @@ public class FvmFacade {
      */
     public <L, A> TransitionSystem<Pair<L, Map<String, Object>>, A, String> transitionSystemFromProgramGraph(
         ProgramGraph<L, A> pg, Set<ActionDef> actionDefs, Set<ConditionDef> conditionDefs) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -369,7 +369,7 @@ public class FvmFacade {
      */
     public <L, A> TransitionSystem<Pair<List<L>, Map<String, Object>>, A, String> transitionSystemFromChannelSystem(
         ChannelSystem<L, A> cs) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -380,7 +380,7 @@ public class FvmFacade {
      * @throws Exception If the code is invalid.
      */
     public ProgramGraph<String, String> programGraphFromNanoPromela(String filename) throws Exception {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -391,7 +391,7 @@ public class FvmFacade {
      * @throws Exception If the code is invalid.
      */
     public ProgramGraph<String, String> programGraphFromNanoPromelaString(String nanopromela) throws Exception {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -402,7 +402,7 @@ public class FvmFacade {
      * @throws Exception If the code is invalid.
      */
     public ProgramGraph<String, String> programGraphFromNanoPromela(InputStream inputStream) throws Exception {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -419,7 +419,7 @@ public class FvmFacade {
      */
     public <Sts, Saut, A, P> TransitionSystem<Pair<Sts, Saut>, A, Saut>
         product(TransitionSystem<Sts, A, P> ts, Automaton<Saut, P> aut) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     
@@ -439,7 +439,7 @@ public class FvmFacade {
      */
     public <S, A, P, Saut> VerificationResult<S> verifyAnOmegaRegularProperty(TransitionSystem<S, A, P> ts,
         Automaton<Saut, P> aut) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -451,7 +451,7 @@ public class FvmFacade {
      * @return An automaton A such that L_\omega(A)=Words(ltl)
      */
     public <L> Automaton<?, L> LTL2NBA(LTL<L> ltl) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
     /**
@@ -463,7 +463,7 @@ public class FvmFacade {
      * @return An equivalent automaton with a single set of accepting states.
      */
     public <L> Automaton<?, L> GNBA2NBA(MultiColorAutomaton<?, L> mulAut) {
-        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+        throw new java.lang.UnsupportedOperationException();
     }
 
 }
